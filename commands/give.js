ce9ee870429bc55a06e31aff49e699c141e8b41d
つぶあん派 <>
2021-04-15 14:24:33 +0000

    バグ修正

--- commands/give.js
+++ commands/give.js
@@ -3,69 +3,59 @@ const Keyv = require('keyv');
 const sqlite = new Keyv('sqlite://db.sqlite', { table: 'db' });
 
 module.exports = {
-	name: 'give',
-	description: '指定ユーザーにEsmileを付与します。',
-	async execute(message, args) {
-		const db = (await sqlite.get(message.author.id)) || {
-			nd: 0,
-			sm: 0,
-			join: 0,
-			fishga: 0,
-			fishco: 0,
-			fishun: 0,
-			fishra: 0,
-			twowc: 0,
-			twolc: 0,
-			twows: 0,
-			twols: 0
-		};
-		const homu = (await sqlite.get('823130006480748556')) || { sm: 0 };
+  name: 'give',
+  description: '指定ユーザーにEsmileを付与します。',
+  async execute(message, args) {
+    const givedb = (await sqlite.get(args[0])) || { nd: 0, sm: 0 };
 
-		if (
-			message.author.id == '755381028817731605' ||
-			message.author.id == '823130006480748556' ||
-			message.author.id == '723052392911863858'
-		) {
-			try {
-				var number = Number(args[1]);
-				if (homu.sm >= number) {
-					db.sm += number;
-					sqlite.set(args[0], db);
-					homu.sm -= number;
-					sqlite.set('823130006480748556', homu);
-					var embed = new Discord.MessageEmbed()
-						.setColor('#0099FF')
-						.setTitle(':white_check_mark: 成功')
-						.setDescription(
-							`<@!${args[0]}> さんに \`${args[1]}sm\` 付与しました。`
-						)
-						.setTimestamp();
-					message.channel.send(embed);
-				} else {
-					var embed = new Discord.MessageEmbed()
-						.setColor('#0099FF')
-						.setTitle(':x: 失敗')
-						.setDescription('Esmileの発行上限に達しています。')
-						.setTimestamp();
-					message.channel.send(embed);
-					return;
-				}
-			} catch (error) {
-				var embed = new Discord.MessageEmbed()
-					.setColor('#0099FF')
-					.setTitle(':x: 失敗')
-					.setDescription(`\`\`\`${error}\`\`\``)
-					.setTimestamp();
-				message.channel.send(embed);
-			}
-		} else {
-			var embed = new Discord.MessageEmbed()
-				.setColor('#0099FF')
-				.setTitle(':x: 失敗')
-				.setDescription('あなたにはこのコマンドを実行する権限がありません。')
-				.setTimestamp();
-			message.channel.send(embed);
-		}
-		return;
-	}
+    if (
+      message.author.id == '755381028817731605' ||
+      message.author.id == '823130006480748556' ||
+      message.author.id == '723052392911863858' ||
+      message.author.id == '638979341371441152'
+    ) {
+      try {
+        if (args[2] === 'nd' || args[2] === 'ndollar') {
+          var number = Number(args[1]);
+          givedb.nd += number;
+          sqlite.set(args[0], givedb);
+          var embed = new Discord.MessageEmbed()
+            .setColor('#0099FF')
+            .setTitle(':white_check_mark: 成功')
+            .setDescription(
+              `<@!${args[0]}> さんに \`${args[1]}nd\` 付与しました。`
+            )
+            .setTimestamp();
+          message.channel.send(embed);
+          return;
+        }
+        var number = Number(args[1]);
+        givedb.sm += number;
+        sqlite.set(args[0], givedb);
+        var embed = new Discord.MessageEmbed()
+          .setColor('#0099FF')
+          .setTitle(':white_check_mark: 成功')
+          .setDescription(
+            `<@!${args[0]}> さんに \`${args[1]}sm\` 付与しました。`
+          )
+          .setTimestamp();
+        message.channel.send(embed);
+      } catch (error) {
+        var embed = new Discord.MessageEmbed()
+          .setColor('#0099FF')
+          .setTitle(':x: 失敗')
+          .setDescription(`\`\`\`${error}\`\`\``)
+          .setTimestamp();
+        message.channel.send(embed);
+      }
+    } else {
+      var embed = new Discord.MessageEmbed()
+        .setColor('#0099FF')
+        .setTitle(':x: 失敗')
+        .setDescription('あなたにはこのコマンドを実行する権限がありません。')
+        .setTimestamp();
+      message.channel.send(embed);
+    }
+    return;
+  }
 };
